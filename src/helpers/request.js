7341430bbaac039b8730920146e50c04df622f50
JayChenFE <chenjiehwy@163.com>
2018-08-15 22:18:03 +0800

    2

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded'
 axios.defaults.baseURL = 'https://note-server.hunger-valley.com'
 axios.defaults.withCredentials = true // 使用跨域请求
 
@@ -13,22 +14,22 @@ export default function request(url, type = 'GET', data = {}) {
         return (status >= 200 && status < 300) || status === 400
       }
     }
+
     if (type.toLowerCase() === 'get') {
       options.params = data
     } else {
       options.data = data
     }
-    axios(options)
-      .then(res => {
-        if (res.status === 200) {
-          resolve(res.data)
-        } else {
-          reject(res.data)
-        }
-      })
-      .catch(err => {
-        console.log(err)
-        console.error({ msg: '网络异常' })
-      })
+
+    axios(options).then(res => {
+      if (res.status === 200) {
+        resolve(res.data)
+      } else {
+        reject(res.data)
+      }
+    }).catch(err => {
+      console.log(err)
+      console.error({ msg: '网络异常' })
+    })
   })
 }
